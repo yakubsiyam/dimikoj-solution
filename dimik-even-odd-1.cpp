0e6381722d1707c1cc531abb65d41cd984457d84
Siyam <67267877+yakubsiyam@users.noreply.github.com>
2022-12-06 17:53:41 +0600

    Even Odd code solution

---
+++ dimik-even-odd-1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int n;
+        cin>>n;
+        (n%2 == 0)
+        ?  cout<<"even"<<endl :
+               cout<<"odd"<<endl;
+
+    }
+    return 0;
+}
