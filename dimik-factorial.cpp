cfe137a2243f63927b5ec82f9f8bbc322dad2cf8
Siyam <67267877+yakubsiyam@users.noreply.github.com>
2022-12-06 17:54:00 +0600

    Factorial code solution

---
+++ dimik-factorial.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        long long int n, fact=1;
+        cin>>n;
+        for(int i=1; i<=n; i++)
+        {
+            fact*= i;
+        }
+        cout<<fact<<endl;
+    }
+    return 0;
+}
